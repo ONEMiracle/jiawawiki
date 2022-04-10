ada4f1c870a3189d21806efec80e91084bbfa2ce
wp <289754190@qq.com>
2022-04-10 10:35:16 +0800

    ceshi

---
+++ src/main/java/com/jiawa/jiawawiki/JiawawikiApplication.java
@@ -0,0 +1,13 @@
+package com.jiawa.jiawawiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiawawikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiawawikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiawa/jiawawiki/JiawawikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.jiawawiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiawawikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
